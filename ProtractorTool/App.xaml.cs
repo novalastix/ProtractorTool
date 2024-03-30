0cf46f7d014df42a42434abbb804bb9300226afd
novalastix <10227076+novalastix@users.noreply.github.com>
2024-03-30 15:07:40 -0700

    Add support for msi installer

--- ProtractorTool/App.xaml.cs
+++ ProtractorTool/App.xaml.cs
@@ -4,13 +4,31 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using System.Threading;
 
 namespace ProtractorTool
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private static readonly Mutex Mutex = new Mutex(true, "35252B40-C1EC-48B0-B796-EB048AC42124");
+        private static MainWindow mainWindow;
+
+        [STAThread]
+        static void Main()
+        {
+            if (Mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                var app = new App();
+                mainWindow = new MainWindow();
+                app.Run(mainWindow);
+                Mutex.ReleaseMutex();
+            }
+            else
+            {
+                mainWindow.WindowState = WindowState.Maximized;
+                mainWindow.Show();
+            }
+        }
+
     }
 }
